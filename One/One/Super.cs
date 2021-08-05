3e0f768e74d6a3c3180356e1c8eac2078a1cf52b
Nikita <nikitabushhan@gmail.com>
2021-08-05 14:48:14 +0300

    dd

--- One/One/Super.cs
+++ One/One/Super.cs
@@ -7,5 +7,10 @@ namespace One
     public class Super
     {
         public string Name { get; set; }
+        public string Surname { get; set; }
+        public Super()
+        {
+            Surname = "dd";
+        }
     }
 }
