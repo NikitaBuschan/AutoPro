caac353f6f8bc7b592efb505ffb5d5165b8fb045
Nikita <nikitabushhan@gmail.com>
2021-08-10 08:50:40 +0300

    sdfsd

--- One/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ One/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace One {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/One;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/One;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
